dca51586d54b7bfa2b19fbdeb84c51283b1710e5
KwonSinwook <1005good@naver.com>
2024-06-18 10:39:17 +0900

    Add > Retry.cs
    간단한 재시작 기능 구현

--- Crossy Road/Assets/Scripts/Retry.cs
+++ Crossy Road/Assets/Scripts/Retry.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Retry : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Restart()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("SampleScene");
     }
 }
